134ef16cfc0fabea6fb71a2bf0b4a6581da026eb
TakahiroDee <hello.gdesignlab@gmail.com>
2017-04-27 03:34:18 +0000

    リファクタ前マージ用

--- routes/web.php
+++ routes/web.php
@@ -14,4 +14,5 @@
 Route::get('/', 'TopController@index');
 Route::get('/ranking', 'RankingController@index');
 Route::get('/ranking/site', 'SiteController@index');
+Route::get('/ranking/itweb', 'ItwebController@index');
 Route::get('/search', 'SearchController@index');
